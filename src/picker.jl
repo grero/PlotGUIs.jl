0d39ef0bafca036410da1808ce7261f0758a8575
roger <roger.herikstad@gmail.com>
2019-01-02 17:31:52 +0800

    Adds textbox for showing which line is highlighted

--- src/picker.jl
+++ src/picker.jl
@@ -1,5 +1,17 @@
 function pick_lines(t::AbstractVector{Float64}, X::Matrix{Float64})
     scene = Scene()
+    poprect = map(scene.px_area) do pa
+        _origin = pa.origin
+        width,height = pa.widths
+        FRect(_origin + Point2f0(0.1*width, 0.9*height), 0.1*width, 0.1*height)
+    end
+    textpos = map(poprect) do pr
+        _origin = pr.origin
+        width,height = pr.widths
+        Point2f0(_origin + Point2f0(0.5*width, 0.5*height))
+    end
+    popup = poly!(campixel(scene), poprect, raw = true, color = :white, strokewidth = 2, strokecolor = :black, visible = true)
+    text!(popup, "1", textsize=30, position=textpos, align=(:center,:center), raw=true)
     linewidths = map(scene.events.mousebuttons) do buttons
         if ispressed(scene, Mouse.left)
             pos = to_world(scene, Point2f0(scene.events.mouseposition[]))
@@ -12,6 +24,7 @@ function pick_lines(t::AbstractVector{Float64}, X::Matrix{Float64})
                     idxm = i
                 end
             end
+            popup[end][1] =  "$(idxm)"
             lw = fill(1.0, size(X,2))
             lw[idxm] = 5.0
         elseif isopen(scene)
