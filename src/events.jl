8b4da52d686c9b05684a74f7b2dc4408826fb345
roger <roger.herikstad@gmail.com>
2019-01-28 13:10:23 +0800

    Fixes some issue with event plot

--- src/events.jl
+++ src/events.jl
@@ -4,15 +4,15 @@ end
 
 Base.display(x,escene::EventScene) = display(x, escene.scene)
 
-function plot_events(data::Matrix{T}, events::Vector{T};fs=30_000.0) where T <: Real
-    zscene = plot_zoom(data[:,1],fs=fs)
-    plot_zoom!(zsene, data[:,2], fs=fs)
+function plot_events(data::Matrix{T}, events::Vector{Int64};fs=30_000.0) where T <: Real
+    zscene = plot_zoom(data[1,:],fs=fs)
+    plot_zoom!(zscene, data[2,:], fs=fs)
     s1 = zscene.scene.children[1].children[1]
-    features = permutedims(data[:,events],[2,1])
+    features = permutedims(Float64.(data[:,events]),[2,1])
     fscene = plot_features(features)
     #connect the selected point feature of fscene
     map(fscene.selected_point) do sp
-        push!(s1[:value], events[sp]) 
+        push!(s1[:value], events[sp]/fs)
     end
     EventsScene(hbox(zscene,fscene))
 end
